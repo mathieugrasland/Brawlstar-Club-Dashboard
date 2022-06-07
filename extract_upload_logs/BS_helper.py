260bf63827f3677b4ebf19523907f1e7d3ad7356
mgrasland <grasland.m@gmail.com>
2022-06-07 09:45:21 +0200

    fix: championshipChallenge type to exclude

--- extract_upload_logs/BS_helper.py
+++ extract_upload_logs/BS_helper.py
@@ -77,7 +77,7 @@ class BS_helper:
                 line["used_tickets"] = 2
                 line["with_club_mate"] = line["points"] in [9, 5]
                 lines.append(line)
-            elif 'mode' in battle_details and battle_details['mode'] != "soloShowdown" and battle_details['mode'] != "duoShowdown" and 'type' in battle_details and battle_details['type'] != 'challenge':
+            elif 'mode' in battle_details and battle_details['mode'] != "soloShowdown" and battle_details['mode'] != "duoShowdown" and 'type' in battle_details and battle_details['type'] != 'challenge' and battle_details['type'] != "championshipChallenge":
                 if 'trophyChange' in battle_details and battle_details['trophyChange'] > 0 and battle_details['trophyChange'] < 5:
                     line["used_tickets"] = 1
                     line["with_club_mate"] = line["points"] in [4, 3]
